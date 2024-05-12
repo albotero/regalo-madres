85c205c45f57a084c97810c6b8226f1e6197632d
Alejandro Botero <alejandro@albotero.com>
2024-05-12 18:39:13 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Option from "./components/Option"
+import bg from "./assets/img/bg.jpeg"
 
-function App() {
+const dests = ["daytona", "bahamas"]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full bg-stone-700 bg-opacity-30">
+      <div
+        className="bg-cover opacity-70 absolute left-0 top-0 w-full bottom-0 -z-10"
+        style={{
+          backgroundImage: `url('${bg}')`,
+        }}
+      />
+      <div className="w-full h-full flex flex-col justify-start items-center gap-16 p-10 pt-20">
+        <span className="font-black text-stone-800 text-4xl text-center">Puedes elegir una de las opciones</span>
+        {dests.map((d, i) => (
+          <Option dest={d} index={i + 1} key={d} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Option.js
@@ -0,0 +1,32 @@
+import React from "react"
+import daytona from "../assets/img/daytona.jpeg"
+import bahamas from "../assets/img/bahamas.jpeg"
+
+const options = {
+  daytona: {
+    name: "Daytona Beach",
+    image: daytona,
+    url: "https://www.booking.com/Share-7q7eiX",
+  },
+  bahamas: {
+    name: "Nassau",
+    image: bahamas,
+    url: "https://www.booking.com/Share-9XUDum",
+  },
+}
+
+const Option = ({ dest, index }) => {
+  const { image, url, name } = options[dest]
+  return (
+    <a className="w-full" href={url}>
+      <div
+        className="w-full aspect-video rounded-xl shadow-black shadow-2xl bg-cover border-2 border-white text-center text-2xl text-blue-950 font-semibold p-3 cursor-pointer"
+        style={{ backgroundImage: `url('${image}')` }}
+      >
+        {index} - {name}
+      </div>
+    </a>
+  )
+}
+
+export default Option
